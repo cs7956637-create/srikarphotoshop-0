fe61291766181bc161184189fabea71c344aad3a
chandras <you@example.com>
2026-09-09 09:28:00 +0530

    fix: hardcode backend production url in axios

--- frontend/vite-project/src/api/axios.js
+++ frontend/vite-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://srikarphotoshop-0.onrender.com/api',
 });
 
 // Pass JWT Token automatically in requests
